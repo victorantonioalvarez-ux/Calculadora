91ca2608a584d6053ceb7c8983809fd13c2cdd4b
Victor <victorantonioalvarez@paucasesnovescifp.cat>
2026-01-28 18:48:53 +0100

    JS hace lo basico, aun no numeros negativos

--- script/calculadora.js
+++ script/calculadora.js
@@ -1,11 +1,65 @@
+// Referencia al input
 const input = document.getElementById("input");
 
+// Variables de guardado
+
+let numeroGuardado = 0;
+let operacionPendiente="";
+
+// Añade numeros
+
 function addNumber(inputNumber){
-	console.log(inputNumber);
-	input.value += inputNumber;
+	if(input.value === "0"){
+		input.value = inputNumber;
+	}else{
+		input.value += inputNumber;
+	}
 }
 
+// Añade operador
 function addOperator(inputOperator){
-	console.log(inputOperator);
-	input.value += inputOperator;
+	numeroGuardado = Number(input.value);
+	operacionPendiente = inputOperator;
+	input.value = "";
+}
+
+// Calculo
+
+function calculate(){
+	let numeroActual = Number(input.value)
+	let resultado = 0;
+
+	if (operacionPendiente === "+") {
+		resultado = numeroGuardado + numeroActual;
+	}else if (operacionPendiente === "-"){
+		resultado = numeroGuardado - numeroActual;
+	}else if (operacionPendiente === "*"){
+		resultado = numeroGuardado * numeroActual;
+	}else if (operacionPendiente === "/"){
+		if (numeroActual === 0){
+			input.value = "ERROR";
+			return
+		}
+		resultado = numeroGuardado / numeroActual;
+	}else {
+		return;
+	}
+
+	input.value =resultado
+} 
+
+// Funciones de borrado
+
+function clearAll(){
+    input.value = "0";
+    numeroGuardado = 0;
+    operacionPendiente = "";
+}
+
+function deleteLast(){
+	if (input.value.length === 1){
+		input.value = "0";
+	}else{
+		input.value = input.value.slice(0, -1);
+	}
 }
